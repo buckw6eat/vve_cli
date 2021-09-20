8cd953d80c4fd68b981d0369c2e30f07f4fdce58
buckw6eat <mistlain@gmail.com>
2021-09-21 02:00:38 +0900

    feat: add `get` using requests

--- src/vve_cli/main.py
+++ src/vve_cli/main.py
@@ -1,5 +1,23 @@
-def hello():
-    print("Hello")
+import requests
+
+
+class VveClient:
+    __urlorigin: str
+
+    def __init__(self) -> None:
+        self.__urlorigin = "http://localhost:50021"
+
+    def get(self, url):
+        return requests.get(self.__urlorigin + url)
+
 
 def run():
-    hello()
+    client = VveClient()
+
+    response = client.get("/version")
+    print(response.status_code)
+    print(response.json().strip())
+
+    response = client.get("/speakers")
+    print(response.status_code)
+    print(response.json())
