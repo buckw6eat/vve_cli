97063ab5a0cdf2ed715a7de2fda372123ba38bf3
buckw6eat <mistlain@gmail.com>
2021-09-22 04:23:38 +0900

    feat: time any response

--- src/vve_cli/main.py
+++ src/vve_cli/main.py
@@ -1,4 +1,14 @@
+import time
+
 import requests
+
+
+class IntervalTimer:
+    def __init__(self) -> None:
+        self.__start = time.perf_counter()
+
+    def elapsed(self) -> float:
+        return round(time.perf_counter() - self.__start, 3)
 
 
 class VveClient:
@@ -14,10 +24,14 @@ class VveClient:
 def run():
     client = VveClient()
 
+    t1 = IntervalTimer()
     response = client.get("/version")
     print(response.status_code)
     print(response.json().strip())
+    print("{:.3f} [sec]".format(t1.elapsed()))
 
+    t2 = IntervalTimer()
     response = client.get("/speakers")
     print(response.status_code)
     print(response.json())
+    print("{:.3f} [sec]".format(t2.elapsed()))
